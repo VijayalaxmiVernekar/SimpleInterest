aaf6c60b4165c97a21003bcd5e0ec1e3d8a1ed69
Vijayalaxmi.Vernekar <Vijayalaxmi.Vernekar@kletech.ac.in>
2025-10-16 16:45:21 +0530

    modified simpleInterest program to take both console and user inputs

--- simple_interest.py
+++ simple_interest.py
@@ -1,5 +1,6 @@
 # simple_interest.py
 # Program to calculate Simple Interest
+import sys
 
 def calculate_simple_interest(principal, rate, time):
     """Calculate simple interest given principal, rate, and time."""
@@ -9,11 +10,18 @@ def calculate_simple_interest(principal, rate, time):
 if __name__ == "__main__":
     print("=== Simple Interest Calculator ===")
     try:
-        principal = float(input("Enter the principal amount: "))
-        rate = float(input("Enter the rate of interest (%): "))
-        time = float(input("Enter the time (in years): "))
+        if len(sys.argv) == 4:
+        # Case 1: Arguments passed (for Jenkins or CLI)
+            p = float(sys.argv[1])
+            r = float(sys.argv[2])
+            t = float(sys.argv[3])
+        else:
+        # Case 2: No arguments passed (for console use)
+            p = float(input("Enter the principal amount: "))
+            r = float(input("Enter the rate of interest (%): "))
+            t = float(input("Enter the time (in years): "))
 
-        interest = calculate_simple_interest(principal, rate, time)
+        interest = calculate_simple_interest(p, r, t)
         print(f"\nSimple Interest = {interest:.2f}")
     except ValueError:
         print("Invalid input! Please enter numeric values.")
